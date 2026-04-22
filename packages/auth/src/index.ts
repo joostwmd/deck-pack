b727d902cf245e48a39b8528ab1ed61fccd2f97b
joostwmd <88150245+joostwmd@users.noreply.github.com>
2026-04-22 20:41:12 +0200

    feat: updated auth methods in better auth package

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -3,6 +3,8 @@ import * as schema from "@deck-pack/db/schema/auth";
 import { env } from "@deck-pack/env/server";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { emailOTP } from "better-auth/plugins";
+import { sendOtpEmail } from "@deck-pack/email";
 
 export function createAuth() {
   const db = createDb();
@@ -10,12 +12,11 @@ export function createAuth() {
   return betterAuth({
     database: drizzleAdapter(db, {
       provider: "pg",
-
       schema: schema,
     }),
     trustedOrigins: env.CORS_ORIGINS,
     emailAndPassword: {
-      enabled: true,
+      enabled: false,
     },
     secret: env.BETTER_AUTH_SECRET,
     baseURL: env.BETTER_AUTH_URL,
@@ -26,7 +27,34 @@ export function createAuth() {
         httpOnly: true,
       },
     },
-    plugins: [],
+    plugins: [
+      emailOTP({
+        async sendVerificationOTP({ email, otp, type }) {
+          if (type === "sign-in") {
+            // Send the OTP for sign in
+            await sendOtpEmail({
+              to: email,
+              otp,
+              type,
+            });
+          } else if (type === "email-verification") {
+            // Send the OTP for email verification
+            await sendOtpEmail({
+              to: email,
+              otp,
+              type,
+            });
+          } else if (type === "forget-password") {
+            // Send the OTP for password reset
+            await sendOtpEmail({
+              to: email,
+              otp,
+              type,
+            });
+          }
+        },
+      }),
+    ],
   });
 }
 
